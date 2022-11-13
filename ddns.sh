e4e27f90508ed174e05b995e51a0798c6b7d9628
Steven de Salas <sdesalas@users.noreply.github.com>
2022-11-13 18:10:17 +0100

    Create ddns.sh

---
+++ ddns.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# Roll your own DDNS
+# Shell script to update namecheap.com dynamic dns
+# for a domain to your external IP address
+#
+# Usage: 
+# $ ddns.sh webmail mydomain.com 7b2af01dca8bed
+
+HOSTNAME=${1:-"$DDNS_HOSTNAME"}
+DOMAIN=${2:-"$DDNS_DOMAIN"}
+PASSWORD=${3:-"$DDNS_PASSWORD"}
+
+IP=`curl -s ipecho.net/plain`
+
+URL="https://dynamicdns.park-your-domain.com/update?host=$HOSTNAME&domain=$DOMAIN&password=$PASSWORD&ip=$IP"
+
+echo "------------------"
+date -u +"%Y-%m-%dT%H:%M:%SZ"
+echo GET $URL
+curl $URL
+printf "\n"
