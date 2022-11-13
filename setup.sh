5232e698c1fc9645a7ea3af2ec02c1912fce61ee
Steven de Salas <sdesalas@users.noreply.github.com>
2022-11-13 18:37:29 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -30,8 +30,8 @@ systemctl status --no-pager cockpit
 
 echo "Step 3: Install Caddy" && sleep 3
 apt install -y debian-keyring debian-archive-keyring
-curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
-curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | gpg --dearmor --always-trust -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | tee /etc/apt/sources.list.d/caddy-stable.list
 apt update
 apt install -y caddy
 systemctl enable --now caddy
